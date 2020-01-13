48ee5d243f95f87f40988baad5540c9d77a6bdd0
arcallan <58698546+arcallan@users.noreply.github.com>
2020-01-13 11:09:50 +0100

    Update kchop1.py

--- kchop1.py
+++ kchop1.py
@@ -1,20 +1,21 @@
+# Binary search using recursive function
 
+# Definition of binary search function
 def binsearch(arr, target, start, end):
+    # Determines midpoint of array
     mid = end-start
-    if arr[mid] == target:
+    # Compares value at midpoint to target value
+    if arr[mid] == target: # Target found
         return print('Target found at index', mid)
-    elif arr[mid] < target:
+    elif arr[mid] < target: # Narrow search to second half of array
         return search(arr, target, mid+1, end)
-    elif arr[mid] > target:
+    elif arr[mid] > target: # Narrow search to first half of array
         return search(arr, target, start, mid-1)
-    else:
+    else: # Target value not in array
         return print('Target not found')
 
-arr = [2, 3, 4, 10, 27, 40, 99, 212]
-target = 212
-ans = 7
-length = len(arr)
+arr = [2, 3, 4, 10, 27, 40, 99, 212] # Array to search
+target = 212 # Target to find
+length = len(arr) # Length of array to be searched
 
-binsearch(arr, target, 0, length-1)
-
-print('Correct answer is ', ans)
+binsearch(arr, target, 0, length-1) # Search function call
