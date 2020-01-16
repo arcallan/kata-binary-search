d687ad82b4a169fc2236d4a7f9c1fbf32da6fe62
arcallan <58698546+arcallan@users.noreply.github.com>
2020-01-16 16:21:31 +0100

    Update kchop1.py

--- kchop1.py
+++ kchop1.py
@@ -3,19 +3,22 @@
 # Definition of binary search function
 def binsearch(arr, target, start, end):
     # Determines midpoint of array
-    mid = end-start
+    mid = round((end+start)/2)
+    print(mid)
     # Compares value at midpoint to target value
     if arr[mid] == target: # Target found
         return print('Target found at index', mid)
     elif arr[mid] < target: # Narrow search to second half of array
-        return search(arr, target, mid+1, end)
+        print("second half")
+        return binsearch(arr, target, mid+1, end)
     elif arr[mid] > target: # Narrow search to first half of array
-        return search(arr, target, start, mid-1)
+        print("first half")
+        return binsearch(arr, target, start, mid-1)
     else: # Target value not in array
         return print('Target not found')
 
-arr = [2, 3, 4, 10, 27, 40, 99, 212] # Array to search
-target = 212 # Target to find
+arr = [2, 3, 4, 10, 11, 27, 40, 99, 212] # Array to search
+target = 11 # Target to find
 length = len(arr) # Length of array to be searched
 
 binsearch(arr, target, 0, length-1) # Search function call
