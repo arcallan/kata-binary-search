ed8b5b95a6082ee037ef5222f3fdad51863a7ab4
arcallan <58698546+arcallan@users.noreply.github.com>
2020-01-16 16:45:34 +0100

    Update kchop2.js

--- kchop2.js
+++ kchop2.js
@@ -3,9 +3,12 @@
 
 // Definition of binary search function
 function binsearch(arr, target, start, end) {
-    mid = (end+start)/2;
+    mid = Math.floor((end+start)/2);
     if (arr[mid] == target) {
         return console.log("The target has index " + mid);
+    }
+    else if ((end - start) < 1) {
+        return console.log("Target not found");
     }
     else if (arr[mid] < target) {
         return binsearch(arr, target, mid+1, end);
@@ -13,14 +16,12 @@ function binsearch(arr, target, start, end) {
     else if (arr[mid] > target) {
         return binsearch(arr, target, start, mid-1);
     }
-    else {
-        return console.log("Target not found");
-    }
+    
 }
 
 var arr = [2, 3, 4, 10, 11, 27, 40, 99, 212];
-var target = 212;
-len = arr.length;
+var target = 11;
+var len = arr.length;
 
 // Function call
 binsearch(arr, target, 0, len-1);
