4eef4b858aef5b2cb7e725ff2a3f95dd2b3ed17e
arcallan <58698546+arcallan@users.noreply.github.com>
2020-01-16 16:44:34 +0100

    Update kchop1.py

--- kchop1.py
+++ kchop1.py
@@ -4,18 +4,16 @@
 def binsearch(arr, target, start, end):
     # Determines midpoint of array
     mid = round((end+start)/2)
-    print(mid)
     # Compares value at midpoint to target value
     if arr[mid] == target: # Target found
         return print('Target found at index', mid)
+    elif end-start < 1: # Down to the last item to search and still haven't found it
+        return print('Target not found')
     elif arr[mid] < target: # Narrow search to second half of array
-        print("second half")
         return binsearch(arr, target, mid+1, end)
     elif arr[mid] > target: # Narrow search to first half of array
-        print("first half")
         return binsearch(arr, target, start, mid-1)
-    else: # Target value not in array
-        return print('Target not found')
+        
 
 arr = [2, 3, 4, 10, 11, 27, 40, 99, 212] # Array to search
 target = 11 # Target to find
